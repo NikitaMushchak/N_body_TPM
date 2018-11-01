0cbbcbe6efae2834e784cd7e7e75676043306d83
NikitaMushchak <39431136+NikitaMushchak@users.noreply.github.com>
2018-11-01 12:54:44 +0300

    Add files via upload

---
+++ source/CaclModule.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+//#include <algorithm>
+
+void ScalePos(std::vector<std::vector<double>>& Particles, double scale);
+
+ void CaclDensity(std::vector<std::vector<double>>&Particles,std::vector<std::vector <std::vector<std::vector<double>>>>&density,
+	double mass,
+	double H,
+	size_t dim);
+
+
+  void CalcPotential(std::vector<
+					std::vector<
+						std::vector<
+							std::vector<double>>>>& rho,
+				   size_t dim);
+ void GetAccel(std::vector<std::vector<double>>& Particles,
+							std::vector<std::vector<std::vector<std::vector<double >>>>& density,
+								std::vector<std::vector<double>>& a,
+									double H);
+
+void Direct(std::vector<std::vector<double>> & Particles, std::vector<std::vector<double>> & dir, double mass);
+double Signum(double  x);
